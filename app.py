07ce0db0c12fa5746a17a33c221d9b844e84f64b
Heart Glico <152345617+Shenheart@users.noreply.github.com>
2023-12-07 12:34:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="NICO_BLOG", page_icon=":wave:", layout="wide")
+st.set_page_config(page_title="HEARTS_BLOG", page_icon=":heart:", layout="wide")
 
 # Define your background image URL from GitHub repository (use the direct link to the raw image file)
 background_image_url = "https://raw.githubusercontent.com/Shenheart/MYWEB/main/PIC.jpg"
@@ -22,13 +22,14 @@ background_style = f"""
 """
 st.markdown(background_style, unsafe_allow_html=True)
 
+img = Image.open("Me.jpg")
+st.image(img, width=600, channels="RGB")
+
+
 st.title("Hi, I'm Shen Heart Glico and Welcome To my Blog :wave:, \n where you'll learn more about me")
 st.header("I'm a 1st Year College Student in BSCPE Course")
 st.write("I'm learning how to code and program")
 st.write("Message me on Facebook [Click here >](https://www.facebook.com/sikuansecret.07")
-
-img = Image.open("Me.jpg")
-st.image(img, width=600, channels="RGB")
 
 st.write("---")
 st.header("About me:")
