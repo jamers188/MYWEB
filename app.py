a6f46422211414a5893b371cbcdccf95ea7debbc
Heart Glico <152345617+Shenheart@users.noreply.github.com>
2023-12-07 13:04:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="SHENHEARTS_BLOG", page_icon=":heart:", layout="wide")
+st.set_page_config(page_title="GLICO BLOG", page_icon=":heart:", layout="wide")
 
 # Define your background image URL from GitHub repository (use the direct link to the raw image file)
 background_image_url = "https://raw.githubusercontent.com/Shenheart/MYWEB/main/PIC.jpg"
@@ -24,7 +24,6 @@ st.markdown(background_style, unsafe_allow_html=True)
 
 img = Image.open("Me.jpg")
 st.image(img, width=600, channels="RGB")
-
 
 st.title("Hi, I'm Shen Heart Glico and Welcome To my Blog :wave:, \n where you'll learn more about me")
 st.header("I'm a 1st Year College Student in BSCPE Course")
